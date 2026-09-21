fd9dbbc9c8cfdfa44e0aaf0c3b43ec4eba6e50c9
Brandon Rodriguez <brandon162001@gmail.com>
2026-09-21 17:12:46 -0500

    update ruta descargas 2

--- Codigo/main.py
+++ Codigo/main.py
@@ -133,13 +133,6 @@ class Ejecutivo(BaseModel):
         self.nombre = data.get("ejecutivo")
         self.celular = data.get("celular_ejecutivo")
 
-# class Compania(BaseModel):
-
-#     def __init__(self, data: dict):
-
-#         self.usuario = data.get("usuario")
-#         self.contrasena = data.get("password")
-
 class Organizacion(BaseModel):
 
     def __init__(self, data: dict):
@@ -186,7 +179,6 @@ class CotizacionContexto:
 
         self.movimiento = data.get("movimiento")
         self.id_cot = data.get("id")
-        #self.compania = Compania(data)
         self.organizacion = Organizacion(data)
         self.vehiculo = Vehiculo(data)
         self.credito = Credito(data)
@@ -196,7 +188,6 @@ class CotizacionContexto:
 
     def __str__(self):
         return pformat({
-            #"Compania": self.compania.to_dict(ocultar=["usuario","contrasena"]),
             "Organizacion": self.organizacion.to_dict(),
             "Vehículo": self.vehiculo.to_dict(ocultar=["num_rodaje","num_motor","num_serie"]),
             "Crédito": self.credito.to_dict(),
